286d6660d8c629ea59e14c533581a1a3dd39f6a3
generyand <generyan.dep@gmail.com>
2024-06-19 17:42:07 +0800

    fixed row height issues

--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -19,9 +19,10 @@ export default function Subject({ data }) {
   return (
     /* FINAL */
     <div
-      className={`bg-red-700 outline outline-red-700 outline-[0.1rem] rounded-sm md:rounded-md bg-opacity-50 text-red-100 p-[0.1rem] md:p-2 flex justify-center items-center  ${subjectStylingClass}`}
+      className={`bg-red-700 outline outline-red-700 outline-[0.1rem] rounded-sm md:rounded-md bg-opacity-50 text-red-100 p-[0.1rem] md:p-2 ${subjectStylingClass}`}
     >
-      {/* <p className="text-xs md:text-base">{subjectStylingClass}</p> */}
+      <p className="text-xs font-bold sm:text-sm md:text-base">{data.title}</p>
+      <p className="text-[.625rem] sm:text-[.75rem] md:text-[.875rem]">{data.timeStart} - {data.timeEnd}</p>
     </div>
 
     /* DEBUGGER 1 */
