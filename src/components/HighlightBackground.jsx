cfb10bc96708a039bf56268c2fd5aaec0b1232de
generyand <generyan.dep@gmail.com>
2024-06-20 17:41:18 +0800

    minor changes in background highlight in form

--- src/components/HighlightBackground.jsx
+++ src/components/HighlightBackground.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HighlightBackground() {
   return (
     <div className="spotlight-background | h-full w-full inset-0 absolute z-[-1] flex justify-end">
-      <div className="blob | z-[-4] bg-cyan-400 opacity-60 w-[7%] h-[15rem] transform rotate-[-45deg] -mt-16 mr-[30%]" />
+      <div className="blob | z-[-4] bg-cyan-400 opacity-60 w-[5%] h-[15rem] transform rotate-[-50deg] -mt-20 mr-[25%] blur-2xl rounded-lg" />
     </div>
   );
 }
