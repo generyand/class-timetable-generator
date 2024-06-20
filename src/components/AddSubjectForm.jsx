a888e7781e38fef0e880f5baa95e39e1844700c8
generyand <generyan.dep@gmail.com>
2024-06-20 19:56:20 +0800

    Minor changes in time component

--- src/components/AddSubjectForm.jsx
+++ src/components/AddSubjectForm.jsx
@@ -68,48 +68,91 @@ function AddSubjectForm({ setAddSubjFormVisible }) {
           />
         </div>
 
-        <div className="time-range-pick">
+        <div className="flex flex-col items-center time-range-pick ">
           <h2 className="mb-2">Time</h2>
-          <div className="flex">
-            <div className="time-start | flex bg-sky-950 items-center text-center rounded-md overflow-hidden">
-              {/* <label htmlFor="time-start-hour">From</label> */}
-              <select
-                name="time-start-hour"
-                id="time-start-hour"
-                className="px-4 py-2 text-white bg-transparent border-none outline-none appearance-none bg-sky-950 focus:outline-none"
-              >
-                <option className="p-4 outline-red-700" selected value="07">
-                  07
-                </option>
-                <option value="08">08</option>
-                <option value="09">09</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-              </select>
-              <p>:</p>
-              <select
-                name="time-start-hour"
-                id="time-start-hour"
-                className="px-4 py-2 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
-              >
-                <option selected value="AM">
-                  00
-                </option>
-                <option value="PM">30</option>
-              </select>
-              <select
-                name="time-start-hour"
-                id="time-start-hour"
-                className="py-2 pl-2 pr-4 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
-              >
-                <option selected value="AM">
-                  AM
-                </option>
-                <option value="PM">PM</option>
-              </select>
+          <div className="flex justify-center w-full inputs">
+            <div className="flex justify-around w-full gap-4 ">
+              <div className="time-start | text-center">
+                <p className="mb-2 text-sm font-light">From</p>
+                <div className="flex items-center overflow-hidden text-center rounded-md bg-sky-950">
+                  <select
+                    name="time-start-hour"
+                    id="time-start-hour"
+                    className="px-4 py-2 text-white bg-transparent border-none outline-none appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option selected value="07">
+                      07
+                    </option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                  <p>:</p>
+                  <select
+                    name="time-start-hour"
+                    id="time-start-hour"
+                    className="px-4 py-2 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option selected value="AM">
+                      00
+                    </option>
+                    <option value="PM">30</option>
+                  </select>
+                  <select
+                    name="time-start-hour"
+                    id="time-start-hour"
+                    className="py-2 pl-2 pr-4 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option selected value="AM">
+                      AM
+                    </option>
+                    <option value="PM">PM</option>
+                  </select>
+                </div>
+              </div>
+              <div className="time-end | text-center ">
+                <p className="mb-2 text-sm font-light">To</p>
+                <div className="flex items-center overflow-hidden text-center rounded-md bg-sky-950">
+                  <select
+                    name="time-end-hour"
+                    id="time-end-hour"
+                    className="px-4 py-2 text-white bg-transparent border-none outline-none appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option value="07">07</option>
+                    <option selected value="08">
+                      08
+                    </option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                  <p>:</p>
+                  <select
+                    name="time-end-hour"
+                    id="time-end-hour"
+                    className="px-4 py-2 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option selected value="AM">
+                      00
+                    </option>
+                    <option value="PM">30</option>
+                  </select>
+                  <select
+                    name="time-end-hour"
+                    id="time-end-hour"
+                    className="py-2 pl-2 pr-4 text-white bg-transparent appearance-none bg-sky-950 focus:outline-none"
+                  >
+                    <option selected value="AM">
+                      AM
+                    </option>
+                    <option value="PM">PM</option>
+                  </select>
+                </div>
+              </div>
             </div>
-            <div className="time-end"></div>
           </div>
         </div>
 
